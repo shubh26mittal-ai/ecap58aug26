2d0b9c6e8e2c1c9ae5734c92a728a048c735b448
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-02 12:32:09 +0530

    Add files via upload

---
+++ ArraysExamples.java
@@ -0,0 +1,237 @@
+//public class ArraysExamples {
+  //  public static void main(String[] args) {
+    //    int a = 10;
+     //   int b = 20;
+       // int c = 30;
+        //int d = 40;
+        //int e = 50;
+       // System.out.println(a);
+        //System.out.println(b);
+        //System.out.println(c);
+        //System.out.println(d);
+        //System.out.println(e);
+        //}
+    //}
+    //import java.util.Arrays;
+    //public class ArraysExamples {
+    //public static void main(String[] args) {
+      //  int[] num = {10, 20, 30, 40, 50};
+        //System.out.println(Arrays.toString(num));//[10, 20, 30, 40, 50]
+    //}
+//}
+    
+   // public class ArraysExamples {
+    //public static void main(String[] args) {
+      //  int num[] = new int[5];
+        //System.out.println(num);//I@2b2fa4f7
+        //System.out.println(Arrays.toString(num));//[0, 0, 0, 0, 0]
+    //}
+//}
+ //import java.util.Arrays;
+ // public class ArraysExamples {
+    //public static void main(String[] args) {
+      //  num[1] = 10;
+        //num[2] = 20;
+        //num[3] = 30;
+        //num[4] = 40;
+        //num[5] = 50;
+        //System.out.println(Arrays.toString(num));//[0, 10, 20, 30, 40, 50]
+    //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+   // public static void main(String[] args) {
+     //   String[] user = {"shubh", "shubh", "shubh", "shubh", "shubh"};
+       // System.out.println(user.length);//5
+        //System.out.println(Arrays.toString(user));//[shubh, shubh, shubh, shubh, shubh]
+    //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+    //public static void main(String[] args) {
+      //  char[] ch = {'a', '4', '@', 'b', 'c'};
+        //System.out.println(ch.length);//5
+        //System.out.println(Arrays.toString(ch));//[a, 4, @, b, c]
+    //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+   // public static void main(String[] args) {
+     //   string[] user = {"shubh", "shubh", "shubh", "shubh", "shubh"};
+       // for(int i=0; i<=user.length-1; i++){
+         //   System.out.println(user[i]);
+        //}
+    //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+   // public static void main(String[] args) {
+     //   for (String x:user) {  //enhanced for loop
+         //   System.out.println(x);
+        //}
+        //System.out.println(Arrays.toString(user));
+    //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+   //public static void main(String[] args) {
+    //int[]n=[6,2,9,4,1,3,5,8,7];
+    //System.out.println(Arrays.toString(n));//[6, 2, 9, 4, 1, 3, 5, 8, 7]
+    //Arrays.sort(n);
+    //System.out.println(Arrays.toString(n));//[1, 2, 3, 4, 5, 6, 7, 8, 9]
+   //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+   // public static void main(String[] args) {
+     //   int[]a= {1,2,3}
+       // int[]b= {1,2,3}
+        //boolean b1= Arrays.equals(a,b);
+        //System.out.println(b1);//true 
+    //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+   // public static void main(String[] args) {
+     //   int[]a= {1,2,3}
+       // int[]b= {1,5,3}
+        //boolean b1= Arrays.equals(a,b);
+        //System.out.println(b1);//false 
+    //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+   // public static void main(String[] args) {
+     //   String[]users= {"shubh", "shubh", "shubh", "shubh", "shubh"};
+       // int a = Arrays.binarySearch(users, key:"shubh");
+        //System.out.println(a);//0
+        //int b= Arrays.binarySearch(users, key:"shubh");
+        //System.out.println(b);//-1
+        //int c= Arrays.binarySearch(users, key:"shubh");
+        //System.out.println(c);//-2
+        //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+   // public static void main(String[] args) {
+     //   int[]a= {1,2,3,4,5};
+       // int[]b= {1,2,3,4,5};
+        //int i1= Arrays.mismatch(a,b);
+        //System.out.println(i1);//-1
+    //}
+//}
+//import java.util.Arrays;
+ 
+ //public class ArraysExamples {
+// public static void main(String[] args) {
+       // int[]a= {1,2,3,};
+        //int[]b= {1,2,4,};
+        //int i1= Arrays.mismatch(a,b);
+        //System.out.println(i1);//2
+    //}
+//}
+//import java.util.Arrays;
+//public class ArraysExamples {
+    //public static void main(String[] args) {
+      //  int[]n1={1,2,3,4,5};
+        //int[]n2=Arrays.copyOfRange(n1, 1, 4);
+        //System.out.println(Arrays.toString(n2));//[2, 3, 4]
+    //}
+//}
+//import java.util.Arrays;
+//public class ArraysExamples {
+  //  public static void main(String[] args) {
+    //    int[]n1={10,20,30,40,50,60,70,80,90};
+      //  int[]n2=Arrays.copyOfRange(n1,from:0,n1.length-1);
+        //System.out.println(Arrays.toString(n2));//[10, 20, 30, 40, 50, 60, 70]
+    //}
+//}
+//import java.util.Arrays;
+//public class ArraysExamples {
+    //public static void main(String[] args) {
+      //  int[]n1={10,20,30,40,50,60,70,80,90};
+        //int[]n2=Arrays.copyOfRange(n1,from:0,n1.length-1);
+        //System.out.println(Arrays.toString(n2));//[10, 20, 30, 40, 50, 60, 70]
+        //int[]n2=Arrays.copyOfRange(n1,from:0,1 to 5);
+        //System.out.println(Arrays.toString(n2));//[10, 20, 30, 40, 50]
+    //}
+//}
+//import java.util.Arrays;
+//public class ArraysExamples {
+//public class Factorial {
+    //public static void main(String[] args) {
+      //  int n = 5;
+        //int factorial = 1;
+        //for (int i = 1; i <= n; i++) {
+          //  factorial = factorial * i;
+        //}
+        //System.out.println(n + "! = " + factorial);
+    //}
+//}
+
+//import java.util.Arrays;
+//public class ArraysExamples {
+  //  public static void main(String[] args) {
+       // int n = 28;
+        //int sum = 0;
+         //for (int i = 1; i < n; i++) {
+           // if (n % i == 0) {
+               // sum = sum + i;
+            //}
+        //}
+        //if (sum == n) {
+           // System.out.println(n + " is a Perfect Number");
+        //} else {
+          //  System.out.println(n + " is NOT a Perfect Number");
+        //}
+    //}
+//}
+
+//public class ArraysExamples {
+  //  public static void main(String[] args) {
+       // System.out.println("Perfect numbers from 1 to 100:");
+        //for (int n = 1; n <= 100; n++) {
+          //  int sum = 0;
+            //for (int i = 1; i < n; i++) {
+              //  if (n % i == 0) {
+                  //  sum = sum + i;
+                    //  }
+                    //}
+            //if (sum == n) {
+              //  System.out.println(n);
+            //}
+        //}
+    //}
+//}
+
+//public class ArraysExamples {
+  //  public static void main(String[] args) {
+       // int n = 7; // number of terms to print
+        //int first = 0, second = 1;
+        //System.out.print("Fibonacci Series: ");
+        //for (int i = 1; i <= n; i++) {
+          //System.out.print(first);
+          //if (i < n) {
+              //  System.out.print(", ");
+            //}
+            //int next = first + second;
+            ////first = second;
+            //second = next;//Fibonacci Series: 0, 1, 1, 2, 3, 5, 8
+        //}
+    //}
+//}
+//public class ArraysExamples {
+  //public static void main(String[] args) {
+//int arr[]={1,2,3,4,5};
+//System.out.println(arr);//[I@2b2fa4f7
+  //}
+//}
---
+++ Loops.java
@@ -0,0 +1,221 @@
+//public class Loops {
+  //  public static void main(String[] args) {
+        //System.out.println("Shubh Mittal");
+        //}
+    //}
+   // public class Loops {
+    //public static void main(String[] args) {
+       // for (int i = 1; i <= 10; i++) {
+         //   System.out.println("Shubh Mittal");
+        //}
+    //}
+    //}
+    //public class Loops {
+    //public static void main(String[] args) {
+        //for (int i = 1; i <= 10; i++) {
+          //  System.out.println(i);
+        //}
+    //}
+    //}
+//public class Loops {
+    //public static void main(String[] args) {
+        //for (int i = 10; i >= 1; i--) {
+            //System.out.println(i);
+        //}
+    //}
+    //}
+//public class Loops {
+  //  public static void main(String[] args) {
+       // int i=1;
+        //while(i<=10){
+           // System.out.println("Shubh Mittal");
+            //i++;
+        //}
+    //}
+    //}
+    //public class Loops {
+    //public static void main(String[] args) {
+       // int i=10;
+        //while(i>=1){
+          //  System.out.println(i);
+            //i--;
+        //}
+    //}
+    //}
+    //public class Loops {
+    //public static void main(String[] args) {
+       // int i=1;
+        //do{
+           // System.out.println("Shubh Mittal");
+            //i++;
+        //}
+        //while(i>=10);
+    //}
+//}
+//public class Loops {
+    //public static void main(String[] args) {
+        //for(int i=1;i<=5;i++){
+            //for(int j=1;j<=5;j++){
+                //System.out.print(j);
+            //}
+            //}
+        //}
+    //
+   // public class Loops {
+    //public static void main(String[] args) {
+        //for(int i=1;i<=10;i++){
+            //if(i==5)break;
+            //System.out.println(i);
+        //}
+    //}
+//}
+//public class Loops {
+    //public static void main(String[] args) {
+       // for(int i=1;i<=10;i++){
+         //   if(i==5)continue;
+           // System.out.println(i);
+        //}
+    //}
+//}
+//public class Loops{
+   // public static void main(String[] args) {
+       // int n = 532;
+        //int count = 0;
+        //int num = n; // preserve original for display
+         //while (num != 0) {
+           // num = num / 10;
+            //count++;
+        //}
+        //System.out.println("Number: " + n);
+        //System.out.println("Number of digits: " + count);
+    //}
+//}
+//public class Loops{
+   //public static void main(String[] args) {
+    //int n = 5325;
+    //int count = 0;
+    //while(n>0){
+        //count++;
+      //  n=n/10;
+    //}
+    //System.out.println(count);
+//}
+//}
+//public class Loops{
+   //public static void main(String[] args) {
+    //int n = 532;
+    //int sum = 0;
+    //while(n>0){
+        //int rem = n%10;
+        //sum = sum + rem;
+        //n=n/10;
+    //}
+    //System.out.println(sum);
+//}
+//}
+//public class Loops{
+  // public static void main(String[] args) {
+    //int n = 121;
+    //int rev = 0;
+    //while(n>0){
+        //int rem = n%10;
+        //rev = rev*10 + rem;
+        //n=n/10;
+    //}
+    //System.out.println(rev);//121
+//}
+//}
+//public class Loops{
+  // public static void main(String[] args) {
+    //int n = 121;
+    //int rev = 0;
+    //while(n>0){
+        //int rem = n%10;
+        //rev = rev + rem;
+        //n=n/10;
+    //}
+    //System.out.println(rev);//4
+//}
+//}
+//public class Loops{
+  // public static void main(String[] args) {
+    //int n = 121;
+    //int rev = 0;
+    //while(n>0){
+        //int rem = n%10;
+        //rev = rev*10 + rem;
+        //n=n/10;
+    //}
+    //System.out.println(rev);//121
+    //System.out.println(n);//0
+    //if (rev==n){
+    //    System.out.println("Palindrome");
+    //}
+    //else{
+      //  System.out.println("Not Palindrome");
+    //}
+//}
+//}
+//public class Loops{
+  // public static void main(String[] args) {
+    //int n = 121;
+    //int temp = 0;
+    //int rev = 0;
+    //while(n>0){
+      //  int rem = n%10;
+        //rev= rev*10 + rem;
+        //n=n/10;
+    //}
+    //System.out.println(rev);//121
+    //if (temp==n){
+      //  System.out.println("Palindrome");
+    //}
+    //else{
+      //  System.out.println("Not Palindrome");
+    //}
+//}
+//}
+//public class Loops{
+    //public static void main(String[] args) {
+      //  int n = 6;
+        //for(int i=1;i<=n;i++){
+          //  if(n%i==0){
+            //    System.out.println(i);
+              //  }
+            //}
+        //}
+    //}
+    //public class Loops{
+    //public static void main(String[] args) {
+      //  int n = 5;
+        //int count = 0;
+        //for(int i=2;i<n;i++){
+          //  if(n%i==0){
+            //    count++;
+            //}
+        //}
+        //if (count==0){
+          //  System.out.println(n+ "is a Primenumber");//5 is a Primenumber
+        //}
+        //else{
+          //  System.out.println(n+ "is not a Primenumber");
+        //}
+    //}
+//}
+//public class Loops{
+  //  public static void main(String[] args) {
+    //    int n = 6;
+      //  int count = 0;
+        //for(int i=2;i<n;i++){
+          //  if(n%i==0){
+            //    count++;
+            //}
+        //}
+        //if (count==0){
+          //  System.out.println(n+ "is a Primenumber");
+        //}
+        //else{
+          //  System.out.println(n+ "is not a Primenumber");//6 is not a Primenumber
+        //}
+    //}
+//}
