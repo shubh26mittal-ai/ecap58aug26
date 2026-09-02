b44c97fe34aa1a705a14600b0737dfd8e19b2b23
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-02 14:11:06 +0530

    Add files via upload

---
+++ MultiDimensionalArrays.java
@@ -0,0 +1,84 @@
+
+//public class MultiDimensionalArrays {
+    //public static void main(String[] args){
+      //  int[][] num = new int[3][3];
+        //System.out.println(num);//[[I@2b2fa4f7
+    //}
+//}
+
+//import java.util.Arrays;
+//public class MultiDimensionalArrays {
+   // public static void main(String[] args){
+     //   int[][] num = new int[3][3];
+       // num[0][0] = 10;
+        //num[0][1] = 20;
+        //num[0][2] = 30;
+        //num[1][0] = 40;
+        //num[1][1] = 50;
+        //num[1][2] = 60;
+        //num[2][0] = 70;
+        //num[2][1] = 80;
+        //num[2][2] = 90;
+        //System.out.println(Arrays.toString(num));//[[I@2b2fa4f7, [I@5c647e05, [I@33909752]
+    //}
+//}
+
+//import java.util.Arrays;
+//public class MultiDimensionalArrays {
+  //  public static void main(String[] args){
+       // for (int i=0;i<num.length;i++){
+         //   for (int j=0;j<num[i].length;j++){
+             //   System.out.print(num[i][j] + " ");
+            //}
+            //System.out.println();
+        //}
+    //}
+//}
+//import java.util.Arrays;
+
+//public class MultiDimensionalArrays {
+  //  public static void main(String[] args){
+    //    String[][] user = {
+              //  {"shubh", "Noida", "meta"},
+                //{"shubh", "Noida", "amazon"},
+               // {"shubh", "meerut", "flipkart"}
+        //};
+        //for (int i=0;i<userDetials.length;i++){
+          //  for (int j=0;j<userDetials.length;j++){
+            //    System.out.print(userDetials[i][j] + " ");
+            //}
+            //System.out.println();
+        //}
+    //}
+
+    //public class MultiDimensionalArrays{
+      //  public static void main(String[]args){
+        //    int [][]num ={
+            //    {10,50},
+              //  {30,60,90},
+                //{80},
+                //{10,20,30,40,50}
+            //};
+            //for (int i=0;i<num.length;i++){
+              //  for (int j=0;j<num[i].length;j++){
+                //    System.out.print(num[i][j] + " ");
+                //}
+                //System.out.println();
+            //}
+        //}
+    //}
+    //import java.util.Arrays;
+
+    //public class MultiDimensionalArrays{
+      //  public static void main(String[]args){
+        //    int [][]a = {{1,2},{3,4}};
+          //  int [][]b = {{1,2},{3,4}};
+            //for (int i=0;i<a.length;i++){
+              //  System.out.println(Arrays.toString(a[i]));
+                //for (int j=0;j<a[i].length;j++){
+                  //  System.out.print(a[i][j] +b[i][j] + " ");
+                //}
+                //System.out.println();
+            //}
+        //}
+    //}
