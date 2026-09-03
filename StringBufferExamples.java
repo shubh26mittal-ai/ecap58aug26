34f808c13fdf83cf4af585bd15ba84ba01659df4
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-03 16:16:01 +0530

    Add files via upload

---
+++ StringBufferExamples.java
@@ -0,0 +1,75 @@
+package basics.string;
+
+public class StringBufferExamples {
+
+	public static void main(String[] args) {
+		//StringBuffer sb = new StringBuffer("Rakesh");
+		//System.out.println(sb);//Rakesh
+		
+		//String s1 = "Ashok";
+		//StringBuffer sb2 = new StringBuffer(s1);
+		//System.out.println(sb2);//Ashok
+		
+		//append()
+		//StringBuffer s1 = new StringBuffer("Shubh");
+		//System.out.println(s1);//Shubh
+		//StringBuffer s2 = new StringBuffer("Mittal");
+		//System.out.println(s2);//Mittal
+		
+		//StringBuffer s3 = s1.append(s2);
+		//System.out.println(s3);//ShubhMittal
+		
+		//insert()
+		//StringBuffer s1 = new StringBuffer("Script");
+		//StringBuffer sb1 = s1.insert(0, "java");
+		//System.out.println(sb1);//javaScript
+		
+		//delete()
+		//StringBuffer s1 = new StringBuffer("JavaScript");
+		//StringBuffer sb1 = s1.delete(4,10 );
+		//System.out.println(sb1);//Java
+		
+		//reverse()
+		//StringBuffer s1 = new StringBuffer("JavaScript");
+		//StringBuffer sb1 = s1.reverse();
+		//System.out.println(sb1);//tpircSavaJ
+		
+		//String s = "Shubh";
+		//system.out.println(s);//Shubh
+		//s.concat("Shubh Mittal");
+		//System.out.println(s);//Shubh Mittal
+		
+		//StringBuffer sb1 = new StringBuffer("Shubh");
+		//System.out.println(sb1);//shubh
+		//sb1.append("Shubh Mittal");
+		//System.out.println(sb1);//Shubh Mittal
+		
+		//String s1 = "Shubh";
+		//String s2 = s1.concat("Shubh");
+		//System.out.println(s1.hashCode());//79866086
+		//System.out.println(s2.hashCode());//-538730560
+		
+		//StringBuffer sb1 = new StringBuffer("Shubh");
+		//StringBuffer sb2 = sb1.append("Shubh");
+		//System.out.println(sb1.hashCode());//1706234378
+		//System.out.println(sb2.hashCode());//1706234378
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+
+		
+
+	}
+
+}
