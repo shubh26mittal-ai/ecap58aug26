61dc08ca8daf814ffdd530ec5985bcb679cc84aa
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-03 14:20:13 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,24 @@
+package basics.string;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		//String s1="excelr";
+		//System.out.println(s1);
+		//String s2="Shubh Mittal";
+		//System.out.println(s2);
+		//String s3 = "shubh";
+		//System.out.println(s3);
+		
+		//literal
+		//String s1="Excelr";
+		//String s2="shubh";
+		//System.out.println(s1);
+		//System.out.println(s2);
+		//System.out.println(System.identityHashCode(s1));1706234378
+		//System.out.println(System.identityHashCode(s2));1867750575
+		
+				
+	}
+
+}
---
+++ StringMethods.java
@@ -0,0 +1,155 @@
+package basics.string;
+import java.util.Arrays;
+public class StringMethods {
+
+	public static void main(String[] args) {
+		//String s1="Shubh";
+		//System.out.println(s1);
+		
+		//charAt index
+		//String s1="excelr";
+		//char ch1 =s1.charAt(3);
+		//System.out.println(ch1);//e
+		
+		//index of character
+		//String s1 = "excelr";
+		//int i1 = s1.indexOf('c');
+		//System.out.println(i1);//2
+		//int i2 = s1.indexOf('t');
+		//System.out.println(i2);//-1
+		//String s3 = "shubh";
+		//int i3 = s1.indexOf('c');
+		//System.out.println(i3);//2
+		//lastindexofchar
+		//String s1 = "excelr";
+		//int i1 = s1.indexOf('c');
+		//System.out.println(i1);//2
+		  //substring(startindex)
+		//String s="javaScript";
+		//String s1=s.substring(0,3);
+		//System.out.println(s1);//jav
+		
+		//String s2=s.substring(0,4);
+		//System.out.println(s2);//java
+		
+		//String s3=s.substring(4,9);
+		//System.out.println(s3);//Scrip
+		
+		//toCharArrays()
+		        //String s = "JavaScript";
+		        //char[] ch = s.toCharArray();
+		        //System.out.println(Arrays.toString(ch));//[J, a, v, a, S, c, r, i, p, t]
+		//parseInt()
+		//String s="123";
+		//System.out.println(s+55);//12355
+		//int i = Integer.parseInt(s);
+		//System.out.println(i);//123
+		//System.out.println(i+55);//178
+		
+		//valueOf()
+		//int i1 =54;
+		//System.out.println(54+6);//60
+		//String s1 = String.valueOf(i1);
+		//System.out.println(s1);//54
+		//System.out.println(s1+6);//546
+		
+		//split()
+		//String s1= "Hello welcome to the String class";
+		//String[] str1 = s1.split("");
+		//System.out.println(Arrays.toString(str1));//[H, e, l, l, o,  , w, e, l, c, o, m, e,  , t, o,  , t, h, e,  , S, t, r, i, n, g,  , c, l, a, s, s]
+
+		
+		//String[] str2 = s1.split(" ");
+		//System.out.println(Arrays.toString(str2));//[Hello, welcome, to, the, String, class]
+		
+		//String[] str3 = s1.split(" ");
+		//System.out.println(Arrays.toString(str3));//[Hello, welcome, to, the, String, class]
+		
+		//concat()
+		//String s1="Hello";
+		//String s2="Bye";
+		//String res=s1.concat(s2);
+		//System.out.println(res);//HelloBye
+		
+		
+		// equals()
+		//String s1 = "Shubh";
+		//String s2 = "Shubh";
+		//boolean b1 = s1.equals(s2);
+		//System.out.println(b1);//true
+		
+		//String s1 = "Shubh";
+		//String s2 = "Demo";
+		//boolean b1 = s1.equals(s2);
+		//System.out.println(b1);//false
+		
+		//equalsIgnoreCase()
+		//String s1 = "Shubh";
+		//String s2 = "Demo";
+		//boolean b1 = s1.equalsIgnoreCase(s2);
+		//System.out.println(b1);//false
+		
+		//String s1 = "Shubh";
+		//String s2 = "Shubh";
+		//boolean b1 = s1.equalsIgnoreCase(s2);
+		//System.out.println(b1);//true
+		
+		//toLowercase()
+		//String s1=("Shubh mittal");
+		//System.out.println(s1.toLowerCase());//shubh mittal
+		
+		//toUppercase()
+		//String s1=("Shubh mittal");
+		//System.out.println(s1.toUpperCase());//SHUBH MITTAL
+		
+		//replace()
+		//String s1 = "Java is a programming language";
+		//String s2 = s1.replace('J','j');
+		//System.out.println(s2);//java is a programming language
+		
+		//String s2 = s1.replace('P','p');
+		//System.out.println(s2);
+		
+		//replaceAll()
+		//String s1 = "Java is a programming language";
+		//String s2 = s1.replaceAll("Java","React");
+		//System.out.println(s2);//React is a programming language
+		
+		//String s2 = s1.replaceAll("Java is a programming language", "Java is a robust language");
+		//System.out.println(s2);//Java is a robust language
+		
+		//getBytes()
+		//String s="Shubh Mittal";
+		//byte[]b1=s.getBytes();
+		//System.out.println(Arrays.toString(b1));//[83, 104, 117, 98, 104, 32, 77, 105, 116, 116, 97, 108]
+		
+		//hashCode()
+		//String s1 = "Shubh";
+		//System.out.println(s1.hashCode());//79866086
+
+		//String s2 = "Shubh";
+		//System.out.println(s2.hashCode());//79866086
+
+		//String s3 = "Shubh";
+		//System.out.println(s3.hashCode());//79866086
+		
+		//trim()
+		//String s = "Shubh";
+		//System.out.println(s.length());
+		//String s1 = s.trim();
+		//System.out.println(s1.length());
+		//System.out.println(s1);//5 5 Shubh
+
+
+
+
+		
+		
+
+
+		
+		
+		
+	}
+}
+
