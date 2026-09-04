bc59a77a67a8c1c0640097f942ff88fd206707b1
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-04 15:03:35 +0530

    Add files via upload

---
+++ MethodsExamples.java
@@ -0,0 +1,61 @@
+package methods;
+
+//public class MethodsExamples {
+	//public void demo(int a) {//method heading 
+		//System.out.println(a);//method body
+	//}
+
+	//public static void main(String[] args) {
+		//MethodsExamples m  =  new MethodsExamples();
+		//m.demo(10);//10 method calling 
+		
+		//System.out.println("Hello welcome to OOP's basics");
+	//}
+	//public static void main(String[]args) {
+		//MethodsExamples m  =  new MethodsExamples();
+		//m.demo();//Hello welcome to OOP's basics
+	
+//public class MethodsExamples {
+
+  //  public void demo(int a) {
+      //  System.out.println(a);
+    //}
+
+    //public int add(int a, int b) {
+      //  return a + b;
+    //}
+
+    //public String fullName(String fName, String lName) {
+      //  return fName + " " + lName;
+    //}
+
+    //public static void main(String[] args) {
+      //  MethodsExamples m = new MethodsExamples();
+
+        //m.demo(10);
+
+        //int x = m.add(5, 10);
+        //System.out.println(x);//15
+
+        //String name = m.fullName("Shubh", "Mittal");
+        //System.out.println(name);//Shubh Mittal
+
+//public class MethodsExamples {
+
+  //  public String test() {
+    //    System.out.println(10);
+      //  return "Hello";
+    //}
+
+    //public static void main(String[] args) {
+      //  MethodsExamples m = new MethodsExamples();
+        //String x = m.test();
+        //System.out.println(x);//Hello
+    //}
+//}
+	
+
+
+	
+
+	   	
