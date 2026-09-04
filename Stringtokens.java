c5c365f3bd2507b8740f1fa7d5cec42db48e8b08
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-04 10:55:28 +0530

    Add files via upload

---
+++ Stringtokens.java
@@ -0,0 +1,37 @@
+package basics.string;
+
+import java.util.StringTokenizer;
+
+public class Stringtokens {
+
+    public static void main(String[] args) {
+       // StringTokenizer st = new StringTokenizer("excelr edtech company pvt ltd");
+
+        //System.out.println(st.countTokens());   // 5
+        //System.out.println(st.hasMoreTokens()); // true
+        //System.out.println(st.nextToken());     // excelr
+
+        //System.out.println(st.hasMoreTokens()); // true
+        //System.out.println(st.nextToken());     // edtech
+
+        //System.out.println(st.hasMoreTokens()); // true
+        //System.out.println(st.nextToken());     // company
+
+        //System.out.println(st.hasMoreTokens()); // true
+        //System.out.println(st.nextToken());     // pvt
+
+        //System.out.println(st.hasMoreTokens()); // true
+        //System.out.println(st.nextToken());     // ltd
+
+        //System.out.println(st.hasMoreTokens()); // false
+
+        // Re-create the tokenizer for the while loop
+        //st = new StringTokenizer("excelr edtech company pvt ltd");
+
+        //while (st.hasMoreElements()) {
+          //  System.out.println(st.nextElement());
+        //}
+    //}
+//}
+
+
