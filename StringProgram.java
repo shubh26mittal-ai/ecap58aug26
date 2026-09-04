76e83ac649c7f8e0fadd24d9da122a98a3f9df6e
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-04 13:44:37 +0530

    Add files via upload

---
+++ StringProgram.java
@@ -0,0 +1,65 @@
+package basics.string;
+
+
+//String s = "Shubh";
+//vowels - a,e,i,o,u,,A,E,I,O,U,
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		//String s ="Shubh";
+		//char ch = s.charAt(0);
+		//System.out.println(ch);//S
+		
+		//if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'
+			//	||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+			//System.out.println(ch + "is a vowel");
+		//}else {
+			//System.out.println(ch + "is a consonant");//Sis a consonant
+		//}
+		//reverse
+		//String s = "excelr";
+		//String s1 = "";
+
+		//for(int i = s.length() - 1; i >= 0; i--) {
+		  //  s1 += s.charAt(i);
+		//}
+
+		//System.out.println(s1);//rlecxe
+		
+		//palindrome
+		
+		String s = "madam";
+		String s1 = "";
+
+		for(int i = s.length() - 1; i >= 0; i--) {
+		   s1 += s.charAt(i);
+		}
+
+		//System.out.println(s1);//madam
+		//if(s.equals(s1)) {
+			//System.out.println(s+"is a palindrome");//madamis a palindrome
+		//}else {
+			//System.out.println(s+"is not a palindrome");
+		
+		//if(s==s1) {
+			//System.out.println(s+"is a palindrome");
+		//}else {
+			//System.out.println(s+"is not a palindrome");//madamis not a palindrome
+		
+		//System.out.println(s.hashCode());//103652732
+		//System.out.println(s1.hashCode());//103652732
+		//if(s==s1) {
+			//System.out.println(s+"is a palindrome");
+		//}else {
+			//System.out.println(s+"is not a palindrome");
+		}
+		
+		
+	}
+			
+		
+
+	}
+
+
