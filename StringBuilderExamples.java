2a31e6a500215ed42d7807e6c44afad3bbef2f0b
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-04 10:03:37 +0530

    Add files via upload

---
+++ StringBuilderExamples.java
@@ -0,0 +1,31 @@
+package basics.string;
+
+import java.util.Arrays;
+
+public class StringBuilderExamples {
+
+	public static void main(String[] args) {
+		//StringBuilder sb = new StringBuilder("Hello");
+		//System.out.println(sb);//Hello
+		
+		//StringBuilder sb1 =new StringBuilder( "Bye");
+		//System.out.println(sb1);//Bye
+		
+		//StringBuilder sb = new StringBuilder("Hello");
+		//System.out.println(sb.charAt(1));//e
+		
+		//StringBuilder sb2 = sb.reverse();
+		//System.out.println(sb2);//olleH
+		
+		//reverse
+		//String s = "Shubh";
+		//String[] str = s.split("");
+		//System.out.println(Arrays.toString(str));//[S, h, u, b, h]
+		//String sv = "";
+		//for(int i=str.length-1;i>=0;i--) {
+			//System.out.println(str[i]);
+			//sv += str[i];//hbuhs
+		//}
+			//System.out.println(sv);//hbuhs
+		//}
+		//}
