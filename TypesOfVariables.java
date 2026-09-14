7719b91d97a4ffc0e15c264f8100d2bf03307890
shubh26mittal-ai <shubh26mittal@gmail.com>
2026-09-14 21:00:44 +0530

    Update TypesOfVariables.java

--- TypesOfVariables.java
+++ TypesOfVariables.java
@@ -1,67 +1,68 @@
 package oops;
 
-//public class TypesOfVariables {
-	//int a = 10;
-	//static int b = 20;
+public class TypesOfVariables {
+	int a = 10;
+	static int b = 20;
 
-	//public static void main(String[] args) {
-		//int c =100;
+	public static void main(String[] args) {
+		int c =100;
 
-//public class TypesOfVariables {//
+public class TypesOfVariables 
+{
 
-  //  int a = 10;
+    int a = 10;
 
-    //public static void main(String[] args) {
-      //  System.out.println(new TypesOfVariables().a);
+    public static void main(String[] args) {
+        System.out.println(new TypesOfVariables().a);
 
-        //TypesOfVariables t = new TypesOfVariables();
-        //System.out.println(t.a);//10
+        TypesOfVariables t = new TypesOfVariables();
+        System.out.println(t.a);//10
 
-//public class TypesOfVariables {//Static variables
+public class TypesOfVariables {//Static variables
 
-   // int a = 10;
+    int a = 10;
 
-    //public static void main(String[] args) {
-      //  System.out.println(new TypesOfVariables().a);
+    public static void main(String[] args) {
+        System.out.println(new TypesOfVariables().a);
 
-        //TypesOfVariables t = new TypesOfVariables();
-        //System.out.println(t.a);
+        TypesOfVariables t = new TypesOfVariables();
+        System.out.println(t.a);
 
-        // ✅ access through object, not class name
-        //System.out.println(t.a);
+         ✅ access through object, not class name
+        System.out.println(t.a);
 
-        // ✅ access through a new object
-        //System.out.println(new TypesOfVariables().a);
+         ✅ access through a new object
+        System.out.println(new TypesOfVariables().a);
 
-//public class TypesOfVariables {
+public class TypesOfVariables {
 
-  //  public void m1(int a) {
-    //    System.out.println(a);
-      //  int b = 50;
-        //System.out.println(b);//50
-    //}
-//public static void main(String[] args) {
-        //TypesOfVariables t = new TypesOfVariables();
-        //t.m1(5);//5 local variables
+    public void m1(int a) {
+        System.out.println(a);
+        int b = 50;
+        System.out.println(b);//50
+    }
+public static void main(String[] args) {
+        TypesOfVariables t = new TypesOfVariables();
+        t.m1(5);//5 local variables
 
-//public class TypesOfVariables {
+public class TypesOfVariables {
 
-  //  public void simple() {
-    //    System.out.println("Non static / instance Methods");//Non static / instance Methods
-    //}
+    public void simple() {
+        System.out.println("Non static / instance Methods");//Non static / instance Methods
+    }
 
-    //public static void test() {
-      //  System.out.println("Static methods");//Static methods
-    //}
+    public static void test() {
+        System.out.println("Static methods");//Static methods
+    }
 
-    //public static void main(String[] args) {
-      //  new TypesOfVariables().simple();
+    public static void main(String[] args) {
+        new TypesOfVariables().simple();
 
-        //TypesOfVariables t = new TypesOfVariables();
-        //t.simple();
+        TypesOfVariables t = new TypesOfVariables();
+        t.simple();
 
-        //TypesOfVariables.test();
-        //test();   // ✅ parentheses added
+        TypesOfVariables.test();
+        test();   // ✅ parentheses added
     }
 }
 
